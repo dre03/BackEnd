f682de211fd1fe3b4f174a38080a01fc03836229
dre03 <andreapriyana23@gmail.com>
2022-10-14 22:08:15 +0700

    Tugas BackEnd

---
+++ tugas_minggu3/Animal.php
@@ -0,0 +1,70 @@
+<?php
+
+# membuat class Animal
+class Animal
+{
+    # property animals
+    public $animals;
+
+    # method constructor - mengisi data awal
+    # parameter: data hewan (array)
+    public function __construct($data)
+    {
+        $this->animals = $data;
+    }
+
+    # method index - menampilkan data animals
+    public function index()
+    {
+        # gunakan foreach untuk menampilkan data animals (array)
+        foreach($this->animals as $animal){
+            echo $animal;
+        }
+    }
+
+    # method store - menambahkan hewan baru
+    # parameter: hewan baru
+    public function store($data)
+    {
+        # gunakan method array_push untuk menambahkan data baru
+        array_push($this->animals, $data);
+    }
+
+    # method update - mengupdate hewan
+    # parameter: index dan hewan baru
+    public function update($index, $data)
+    {
+        $this->animals[$index] = $data;
+    }
+
+    # method delete - menghapus hewan
+    # parameter: index
+    public function destroy($index)
+    {
+        # gunakan method unset atau array_splice untuk menghapus data array
+        array_splice($this->animals,$index,1);
+    }
+}
+
+# membuat object
+# kirimkan data hewan (array) ke constructor
+$animal = new Animal(["kucing ","kambing ","rusa "]);
+
+echo "Index - Menampilkan seluruh hewan <br>";
+$animal->index();
+echo "<br>";
+
+echo "Store - Menambahkan hewan baru <br>";
+$animal->store('burung');
+$animal->index();
+echo "<br>";
+
+echo "Update - Mengupdate hewan <br>";
+$animal->update(2, 'Sapi ');
+$animal->index();
+echo "<br>";
+
+echo "Destroy - Menghapus hewan <br>";
+$animal->destroy(1);
+$animal->index();
+echo "<br>";
---
+++ tugas_minggu3/variabel.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Andre Apri";
+$jurusan = "Teknik Informatika";
+$umur = 19;
+$ipk = 4.0;
+$isMarrid = false;
+
+echo "$nama";
+echo "<br>";
+echo "$jurusan";
+echo "<br>";
+echo "$umur";
+echo "<br>";
+echo "$ipk";
+echo "<br>";
+echo "$isMarrid";
