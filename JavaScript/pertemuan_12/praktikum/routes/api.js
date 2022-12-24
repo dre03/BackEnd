6b0d837fa278baa8d7aa196aa8fe720de59fbe64
dre03 <andreapriyana23@gmail.com>
2022-12-24 13:54:09 +0700

    task peremuan 12

---
+++ JavaScript/pertemuan_12/praktikum/routes/api.js
@@ -0,0 +1,23 @@
+// import StudentController
+const StudentController = require("../controllers/StudentController");
+
+// import express
+const express = require("express");
+
+// membuat object router
+const router = express.Router();
+
+/**
+ * Membuat routing
+ */
+router.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+router.get("/students", StudentController.index);
+router.post("/students", StudentController.store);
+router.put("/students/:id", StudentController.update);
+router.delete("/students/:id", StudentController.destroy);
+
+// export router
+module.exports = router;
